2244477a78091206487ed8e8e08f4415223a8b7e
Laasya Gadi <62540735+laasya15g@users.noreply.github.com>
2020-06-22 14:33:21 +0530

    uploaded file_extension.py
    both outputs are present

--- file_extension.py
+++ file_extension.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import math
+r=float(input("enter the radius of circle:"))
+area= math.pi * r**2
+print("the area of circle with radius","%0.2f"%r, "is:","%f"%area)
+
+
+# In[16]:
+
+
 import os
 file_name=input("enter the filename")
 extension=file_name.split(".")
@@ -9,3 +24,10 @@ elif extension[-1]=='png':
     print("portable network graphics")
 else:
     print("none")
+
+
+# In[ ]:
+
+
+
+
